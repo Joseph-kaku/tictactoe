1669e6b57825e5d8f6831123d1fdb44ceb90a436
Joseph-kaku <samkaks18@gmail.com>
2022-10-26 20:32:18 -0600

    completed project

--- main.cpp
+++ main.cpp
@@ -87,7 +87,73 @@ int main()
 
     do
     {
-        board()
-        
+        board();
+        player=(player%2)?1:2;
+
+        cout << "Player " << player << ", enter the number: ";
+        cin >> choice;
+
+        mark=(player == 1) ? 'X' : 'O';
+
+        if(choice == 1 && square[1] == '1')
+        {
+            square[1] = mark;
+        }
+        else if(choice == 2 && square[2] == '2')
+        {
+            square[2] = mark;
+        }
+        else if(choice == 3 && square[3] == '3')
+        {
+            square[3] = mark;
+        }
+        else if(choice == 4 && square[4] == '4')
+        {
+            square[4] = mark;
+        }
+        else if(choice == 5 && square[5] == '5')
+        {
+            square[5] = mark;
+        }
+        else if(choice == 6 && square[6] == '6')
+        {
+            square[6] = mark;
+        }
+        else if(choice == 7 && square[7] == '7')
+        {
+            square[7] = mark;
+        }
+        else if(choice == 8 && square[8] == '8')
+        {
+            square[8] = mark;
+        }
+        else if(choice == 9 && square[9] == '9')
+        {
+            square[9] = mark;
+        }
+        else 
+        {
+            cout << "INVALID MOVE!";
+            
+            player --;
+            cin.ignore();
+            cin.get();
+        }
+
+        i = checkwin();
+        player++;
     }
+    while(i == -1);
+    board();
+    if(i == 1)
+    {
+        cout << "\aCONGRATULATIONS! PLAYER  " << --player << " WINS!";
+    }
+    else
+    {
+        cout << "\a GAME DRAW!";
+    }
+    cin.ignore();
+    cin.get();
+    return 0;
 }
